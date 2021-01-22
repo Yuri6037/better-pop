b05b8305c3d7a04e96462610390ae6a71eb2490a
Yuri Edward <yuri6037@outlook.com>
2021-01-22 20:20:24 +0100

    Added automatic script to install WhiteSur theme

---
+++ install_whitesur.sh
@@ -0,0 +1,20 @@
+#Install requirements
+sudo apt install sassc
+
+#Download and inject OSX theme
+git clone https://github.com/Yuri6037/WhiteSur-gtk-theme.git
+cd WhiteSur-gtk-theme
+./install.sh -i void -p 25 -c light --nautilus-use-colors
+sudo ./install.sh -g -c light
+
+#Inject flatpak
+sudo flatpak override --filesystem=~/.themes
+flatpak override --user --filesystem=~/.themes
+
+#Cleanup
+sudo rm -rf /usr/share/themes/WhiteSur*
+cd ..
+rm -rf WhiteSur-gtk-theme
+
+#Uninstall requirements
+sudo apt purge sassc
